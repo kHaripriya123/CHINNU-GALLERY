3592df76b618020f3824515b3c76f987a981834c
kHaripriya123 <kotagiriharipriya888@gmail.com>
2024-07-21 15:54:47 +0530

    red-button created in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,51 @@
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../Utilities/Firebase";
 import logo from "../KRIYAASH.svg";
+import { Button } from "@material-tailwind/react";
+
 const Header = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
 
-const handleSignOut = () => {
+  const handleSignOut = () => {
 
-    
-signOut(auth).then(() => {
-  // Sign-out successful.
 
-  navigate("/login");
+    signOut(auth).then(() => {
+      // Sign-out successful.
 
-}).catch((error) => {
-  // An error happened.
+      navigate("/login");
 
-});
-}
+    }).catch((error) => {
+      // An error happened.
 
-return (
+    });
+  }
 
-  <>
-            <div className="flex justify-between">
-            <div className="">
-            <img  src={logo} className=" w-44 pb-6" alt="logo" />
-            </div>
-            <div className="Nav-Items py-4">
-                <ul className="flex">
+  return (
 
-                    <li className="px-4 font-bold">HOME</li>
-                  
-                    <li className="px-4 underline blue-500 font-bold "><Link to = "Contact">CONTACT</Link></li>
-                    <li className="px-4 font-bold">CART</li>
-                    <li className="px-4 underline blue-500 font-bold"><Link to = "IPLTABLE">IPLTABLE</Link></li>
-                    <li className="px-4 underline blue-500 font-bold "><Link to = "ITCities">ITCITIES</Link> </li>
-                     <button className="bg-indigo-700 rounded-lg px-1 font-bold" onClick={handleSignOut}>SIGN OUT</button>
-                    </ul>
-                   
-                    </div>
-                    </div>
-                  
-                    </>
-        )
+    <>
+      <div className="flex justify-between">
+        <div className="">
+          <img src={logo} className=" w-44 pb-6" alt="logo" />
+        </div>
+        <div className="Nav-Items py-4">
+          <ul className="flex">
+            <Button color="red">color red</Button>
+            <li className="px-4 font-bold">HOME</li>
+
+            <li className="px-4 underline blue-500 font-bold "><Link to="Contact">CONTACT</Link></li>
+            <li className="px-4 font-bold">CART</li>
+            <li className="px-4 underline blue-500 font-bold"><Link to="IPLTABLE">IPLTABLE</Link></li>
+            <li className="px-4 underline blue-500 font-bold "><Link to="ITCities">ITCITIES</Link> </li>
+            <button className="bg-indigo-700 rounded-lg px-1 font-bold" onClick={handleSignOut}>SIGN OUT</button>
+          </ul>
+
+        </div>
+      </div>
+
+    </>
+  )
 }
 export default Header;
 
